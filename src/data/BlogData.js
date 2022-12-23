bdc82d97a6e1478bf004d4d9a68cdbac477471e6
jorlar <jqern.larsen@gmail.com>
2022-12-23 10:24:16 +0100

    :sparkles: Finish project

---
+++ src/data/BlogData.js
@@ -0,0 +1,12 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"My DEV RETRO 2022",
+        tags:["Programming Journey","Javascript","IwriteCode"],
+        date:"16 Dec, 2022",
+        imgSrc:"https://jornlarsen.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1671541414778%2FnMDm1VHJV.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
+        link:"https://jornlarsen.hashnode.dev/how-i-got-to-where-i-am-today-where-it-all-started-devretro2022"
+    }
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,52 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"GitHub Finder",
+        description:"A fun little project created to search and lookup users on Github, built with React.js",
+        tags:["react","SASS","styledComponents"],
+        
+        demo:"http://ablaze-farmer02.surge.sh/",
+        github:"https://github.com/jorlar/Git_find"
+    },
+    {
+        id:2,
+        name:"Iprosess AS",
+        description:"Fullfledge website with online store, made for a norwegian leadership coach",
+        tags:["Wordpress","CSS","Icommerce"],
+        
+        demo:"http://www.iprosess.no"
+        
+    },
+    {
+        id:3,
+        name:"Samina Berntsen",
+        description:"A fullfledge website built with Wordpress with full onlinestore using Klarna and Vipps Checkout",
+        tags:["Wordpress","api","Icommerce","Klarna Checkout","Vipps"],
+        
+        demo:"http://www.saminaberntsen.no/"
+    },
+    {
+        id:4,
+        name:"Webiste Mockup",
+        description:"A cool looking mockup website built with React.js, SASS and styled components.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://fantastic-fairy-40f13c.netlify.app/",
+        github:"https://github.com/jorlar/website_fake"
+       
+    },{
+        id:5,
+        name:"Jotun Utvikling AS",
+        description:"Website built for a norwegian development company",
+        tags:["react","redux","framer-motion", "sass"],
+        
+        demo:"http://www.jotun-utvikling.no",
+        github:"https://github.com/jorlar/Jotun_utvikling_master"
+        
+    },
+    
+    
+  
+] 
