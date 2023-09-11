4e46bd6cf467d9dab4e9d788f4d9c4cd9510f1bb
jorlar <jqern.larsen@gmail.com>
2023-09-11 10:47:12 +0200

    :sparkles: Add new project to work-page

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,52 +1,60 @@
 // work data
 
 export const Work = [
-    {
-        id:1,
-        name:"GitHub Finder",
-        description:"A fun little project created to search and lookup users on Github, built with React.js",
-        tags:["react","SASS","styledComponents"],
-        
-        demo:"http://ablaze-farmer02.surge.sh/",
-        github:"https://github.com/jorlar/Git_find"
-    },
-    {
-        id:2,
-        name:"Iprosess AS",
-        description:"Fullfledge website with online store, made for a norwegian leadership coach",
-        tags:["Wordpress","CSS","Icommerce"],
-        
-        demo:"http://www.iprosess.no"
-        
-    },
-    {
-        id:3,
-        name:"Samina Berntsen",
-        description:"A fullfledge website built with Wordpress with full onlinestore using Klarna and Vipps Checkout",
-        tags:["Wordpress","api","Icommerce","Klarna Checkout","Vipps"],
-        
-        demo:"http://www.saminaberntsen.no/"
-    },
-    {
-        id:4,
-        name:"Webiste Mockup",
-        description:"A cool looking mockup website built with React.js, SASS and styled components.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://fantastic-fairy-40f13c.netlify.app/",
-        github:"https://github.com/jorlar/website_fake"
-       
-    },{
-        id:5,
-        name:"Jotun Utvikling AS",
-        description:"Website built for a norwegian development company",
-        tags:["react","redux","framer-motion", "sass"],
-        
-        demo:"http://www.jotun-utvikling.no",
-        github:"https://github.com/jorlar/Jotun_utvikling_master"
-        
-    },
-    
-    
-  
-] 
+  {
+    id: 1,
+    name: "GitHub Finder",
+    description:
+      "A fun little project created to search and lookup users on Github, built with React.js",
+    tags: ["react", "SASS", "styledComponents"],
+
+    demo: "http://ablaze-farmer02.surge.sh/",
+    github: "https://github.com/jorlar/Git_find",
+  },
+  {
+    id: 2,
+    name: "Iprosess AS",
+    description:
+      "Fullfledge website with online store, made for a norwegian leadership coach",
+    tags: ["Wordpress", "CSS", "Icommerce"],
+
+    demo: "http://www.iprosess.no",
+  },
+  {
+    id: 3,
+    name: "Samina Berntsen",
+    description:
+      "A fullfledge website built with Wordpress with full onlinestore using Klarna and Vipps Checkout",
+    tags: ["Wordpress", "api", "Icommerce", "Klarna Checkout", "Vipps"],
+
+    demo: "http://www.saminaberntsen.no/",
+  },
+  {
+    id: 4,
+    name: "Webiste Mockup",
+    description:
+      "A cool looking mockup website built with React.js, SASS and styled components.",
+    tags: ["react", "styledComponents", "react-router"],
+
+    demo: "https://fantastic-fairy-40f13c.netlify.app/",
+    github: "https://github.com/jorlar/website_fake",
+  },
+  {
+    id: 5,
+    name: "Jotun Utvikling AS",
+    description: "Website built for a norwegian development company",
+    tags: ["react", "redux", "framer-motion", "sass"],
+
+    demo: "http://www.jotun-utvikling.no",
+    github: "https://github.com/jorlar/Jotun_utvikling_master",
+  },
+  {
+    id: 6,
+    name: "NextJS Static Blog",
+    description: "Static Blogpage built using NextJS ",
+    tags: ["NextJS", "Typescript", "react", "sass"],
+
+    demo: "https://next-js-blog-phi-beige.vercel.app/",
+    github: "https://github.com/jorlar/next-js-blog",
+  },
+];
